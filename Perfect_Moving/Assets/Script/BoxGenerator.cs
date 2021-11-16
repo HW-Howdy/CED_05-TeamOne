3b8312ec7b374df9001878cbe113b99cacf5b25e
HW-Howdy <84364883+HW-Howdy@users.noreply.github.com>
2021-11-16 20:51:32 +0900

    Perfect Moving Test version_0.2
    2188002 신성명
    대대적인 수정이 있었습니다.
    대략적인 정보는 다음과 같고 자세한 정보는 그 아래에 기술하겠습니다.
    
    1. Asset 밑에 있던 파일들의 정리
    2. 파일 이름의 수정
    3. 스크립트 파일의 수정 및 주석 추가
    
    1.  Asset 폴더 밑에 있던 파일들을 각 역할에 맞게 정리하였습니다.
    새로 만들 파일들도 같은 형식을 유지해주시면 감사하겠습니다.
    현재 있는 폴더만으로 구분이 어려운 파일은 새 폴더를 만들어 분류해주세요.
    
    2. 파일들의 이름을 일관적으로 통일하였습니다.
    이름 형식도 동일하게 유지해야 팀 단위의 작업에서 혼선이 줄 것입니다.
    앞으로도 현재 형식을 지켜주시면 감사하겠습니다.
    
    3. 작성되어있는 4 개의 스크립트를 전체적으로 정리하였습니다.
    논리 오류가 있는 구문이나 최적화가 가능한 부분은 모두 수정하였고, 그에 따른 주석을 전부 기입하였습니다.
    자신이 쓴 코드를 남이 이해하기 쉽도록 주석을 달아주시면 감사하겠습니다.
    수정된 부분에 대해서는 업데이트 로그를 참고하시면 되겠습니다.
    
    Co-Authored-By: znlfxm <63768288+znlfxm@users.noreply.github.com>
    Co-Authored-By: ksk4303 <94444074+ksk4303@users.noreply.github.com>

---
+++ Perfect_Moving/Assets/Script/BoxGenerator.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxGenerator : MonoBehaviour
+{
+    public GameObject boxPrefab;            //떨어지는 블럭
+    private float span = 1.0f;              //재생성에 걸리는 시간
+    private float delta = 1.0f;             //현재 지난 시간
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        delta += Time.deltaTime;            //시간 계산
+        if (delta > span)                   //기준 시간을 지나면 작동
+        {
+            delta = 0;
+            GameObject go = Instantiate(boxPrefab);         //프리팹 소환
+            int px = Random.Range(-5, 5);                   //x좌표를 랜덤으로 지정
+            go.transform.position = new Vector3(px, 6, 0);  //만든 프리팹을 이동
+        }
+    }
+}
---
+++ Perfect_Moving/Assets/Script/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController: MonoBehaviour
+{
+    private int life = 3;               //목숨
+
+    private Rigidbody2D rigid2D;        //리기드바디
+
+    private float jumpForce = 340.0f;   //점프가속도
+    private float walkForce = 2.0f ;    //이동가속도
+    private float maxWalkSpeed = 3.0f;  //최대속도
+    private int isJump = 1;             //현재 남은 점프 수
+    private int maxJump = 1;            //최대 점프 횟수
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid2D = GetComponent<Rigidbody2D>(); //리기드바디 생성
+    }
+
+
+    public void Hit()           //적과 부딪히면 호출되는 함수
+    {
+        life--;                 //생명 1 감소
+        if (life == 0) Dead();  //만약 0이라면 사망
+    }
+
+
+    public void Dead()          //사망시 호출되는 함수
+    {
+        Destroy(gameObject);    //이 오브젝트를 파괴함
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)                             //무언가와 부딪힐 때 호출되는 함수
+    {
+        if (collision.CompareTag("Enemy")) Hit();                                   //적과 부딪히면 생명이 깎임
+        if (collision.CompareTag("Floor")&&(isJump != maxJump)) isJump = maxJump;   //바닥과 맞닿으면 점포 횟수 충전
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)&&(isJump != 0))                     //점프키(space)를 누른 상태 및 점프 횟수가 남아있으면
+        {
+            rigid2D.AddForce(transform.up * jumpForce);                         //위쪽 방향으로 점프가속도만큼 가속함
+            isJump -= 1;                                                        //점프 횟수 -1
+        }
+
+        float key = Input.GetAxis("Horizontal");                                //방향을 받아옴. 우측 함수는 a / 왼쪽 화살표 입력에는 -1, d / 오른쪽 화살표 입력에는 1을 반환함
+        rigid2D.AddForce(transform.right * key * walkForce);
+
+        //현재 속력이 최대 속력보다 빠르면 재조정함
+        if (Mathf.Abs(rigid2D.velocity.x) > this.maxWalkSpeed) rigid2D.velocity = new Vector2(key * maxWalkSpeed, rigid2D.velocity.y);
+
+        if (key != 0) transform.localScale = new Vector3(key, 1, 1);            //가만히 서있다면 앞으로 향하게 회전함. 현재는 2D 이미지라 어색해보임
+
+        //떨어진다면 씬을 새로 로드함
+        if (transform.position.y < -10) SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+
+    public int LIFE             //private 변수 life에 접근하기 위한 생성자
+    {
+        get { return life; }    
+	}
+}
