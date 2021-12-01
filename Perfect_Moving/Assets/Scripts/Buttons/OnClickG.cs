efa303e9bed5f13ca3f05118d7159d9be6ce35fb
HW-Howdy <84364883+HW-Howdy@users.noreply.github.com>
2021-12-01 19:04:05 +0900

    Perfect Moving version_0.11.1
    2188002 신성명
    
    변경 및 추가:
      즐거운 부활절 되세요!
      연구원들의 메모 완성
    
    수정된 버그:
      플레이어가 좌우 벽에 점프하며 부딪히면 낙하 속도가 줄거나 투명벽에 그대로 박힘. (충돌 연산)

---
+++ Perfect_Moving/Assets/Scripts/Buttons/OnClickG.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OnClickG : MonoBehaviour
+{
+
+    [SerializeField]
+    private GameObject music;
+    [SerializeField]
+    private GameObject hiddenMusic;
+    [SerializeField]
+    private Text hiddenText;
+
+    private bool showUP = false;
+
+    private float nowTime = 0.0f;
+    private readonly float time = 5.0f;
+
+    public void ClickGButton() {
+        music.SetActive(false);
+        hiddenMusic.SetActive(true);
+        showUP = true;
+    }
+
+	private void Update() {
+		if(showUP&&(nowTime <= time)) {
+            nowTime += Time.deltaTime;
+            hiddenText.color = new Color(1, 1, 1, 1 - (time - nowTime)/time);
+		}
+	}
+}
