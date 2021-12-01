d434faa43d92110a0c1b1762f8c6daadddfed368
HW-Howdy <84364883+HW-Howdy@users.noreply.github.com>
2021-12-01 15:27:47 +0900

    Perfect Moving version_0.11.0
    2188002 신성명
    
    변경 및 추가:
      배경음악 추가 (저작권 문제 없는 것들로)
      GameOver 타이틀에 연구원들의 코멘트 추가(추가 중)

--- Perfect_Moving/Assets/Scripts/Managers/GameResultManager.cs
+++ Perfect_Moving/Assets/Scripts/Managers/GameResultManager.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +10,15 @@ public class GameResultManager : MonoBehaviour
 	private Text nowResultText;
 	[SerializeField]
 	private Text highResultText;
+	[SerializeField]
+	private Text sayingResultText;
+	[SerializeField]
+	private Font[] Fonts;
 
 	private float nowTime;
 	private float highTime;
+
+	private readonly string xmlFileName = "XML/ResearcherSaying";
 
 	private void Start() {
 		nowTime = PlayerPrefs.GetFloat("NowScore");
@@ -21,6 +28,53 @@ public class GameResultManager : MonoBehaviour
 		nowResultText.text = "이번 실험 시간 | " + ((int)(nowTime / 60)).ToString() + " : " + (Mathf.Round((nowTime * 10) % 600) / 10).ToString();
 		highResultText.text = "이전 최고 시간 | " + ((int)(highTime / 60)).ToString() + " : " + (Mathf.Round((highTime * 10) % 600) / 10).ToString();
 
-		if(nowTime > highTime) PlayerPrefs.SetFloat("highScore", nowTime);
+		int randomNum = Random.Range(0, Fonts.Length);
+
+		TextAsset txtAsset = (TextAsset)Resources.Load(xmlFileName + randomNum.ToString());
+		XmlDocument xmlDoc = new XmlDocument();
+		xmlDoc.LoadXml(txtAsset.text);
+
+		sayingResultText.font = Fonts[randomNum];
+		sayingResultText.text = RandomText(randomNum, xmlDoc);
+		if (nowTime > highTime) PlayerPrefs.SetFloat("HighScore", nowTime);
+	}
+
+	private string RandomText(int Num, XmlDocument xml) {
+		if (nowTime <= 10) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type1");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else if (nowTime <= 20) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type2");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else if (nowTime <= 60) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type3");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else if (nowTime <= 120) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type4");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else if (nowTime <= 180) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type5");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else if (nowTime <= 300) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type6");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else if (nowTime <= 600) {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type7");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		else {
+			XmlNode cost_Table = xml.SelectSingleNode("dataroot/type8");
+			return cost_Table.SelectNodes("saying")[Random.Range(0, cost_Table.SelectNodes("saying").Count)].InnerText;
+		}
+		//		0 "총 책임자. 차가움";
+		//		1 "중간 관리자. 무덤덤";
+		//		2 "실험 진행자. 구어체";
+		//		3 "동료 실험자. 사무적";
 	}
 }
